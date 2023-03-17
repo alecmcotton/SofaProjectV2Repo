1ab7faf46f8692a954b373e6a07743d09f6a4948
Alec Cotton <aleccotton01@gmail.com>
2023-03-16 21:21:06 -0400

    Initial Commit

---
+++ Source/SofaProjectV2/SofaProjectV2.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SofaProjectV2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SofaProjectV2, "SofaProjectV2" );
+ 
---
+++ Source/SofaProjectV2/SofaProjectV2GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SofaProjectV2GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASofaProjectV2GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASofaProjectV2GameMode();
+};
+
+
+
